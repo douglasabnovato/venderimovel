14b658524ce96223b1649296b526eabccf9e6773
@douglasabnovato <douglasabnovato@gmail.com>
2019-03-09 13:49:25 -0300

    criando projeto

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+//E criamos o primeiro componente personalizado App
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return ( 
+            <div>
+                <h1> @douglasabnovato</h1> 
+            </div>
+        );
+    }
+}
