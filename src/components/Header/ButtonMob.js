251012d93d89991bb9c9d2d54320436906e3e332
@douglasabnovato <douglasabnovato@gmail.com>
2019-03-09 18:10:45 -0300

    spa - implementar botão de mobile

--- src/components/Header/ButtonMob.js
+++ src/components/Header/ButtonMob.js
@@ -1,12 +1,42 @@
 import React from 'react';
 
+{/* 
+    criaremos os três retângulos paralelos no nosso programa de design, no caso Fireworks. 
+    Iremos exportar esse botão como png e fundo transparente.
+    No componente ButtonMob faremos o método clickMe que fará a função toggle 
+*/}
+
 export default class ButtonMob extends React.Component {
 
-    render(){
-
-        return (
-            null
-        )
+    /* estado */
+    constructor()
+    {
+        super();
+        this.state={
+            visNav:'flex'
+        }
     }
 
+    /* Fará a função de toggle */
+    clickMe()
+    {
+        if(this.state.visNav === 'flex'){
+            this.setState({
+                visNav:'none'
+            });
+        }else{
+            this.setState({
+                visNav:'flex'
+            });
+        }
+        document.querySelector(".nav").style.display=this.state.visNav;
+    }
+
+    render(){
+        return(
+            <div className="buttonMob" onClick={this.clickMe.bind(this)}>
+                <img src="/img/icon-menu-48-48.png" alt="Botão Mobile"/>
+            </div>
+        );
+    }
 }
