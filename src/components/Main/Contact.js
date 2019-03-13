e9658b7709386e51ddaa2acdbbb2ead58577916e
@douglasabnovato <douglasabnovato@gmail.com>
2019-03-13 19:30:23 -0300

    #9 formulário de contato

--- src/components/Main/Contact.js
+++ src/components/Main/Contact.js
@@ -5,8 +5,14 @@ export default class Contact extends React.Component {
     render(){
 
         return (
-            null
+            <div className="formulario">
+                <h1>Fale conosco</h1>
+                <form name="form1" id="form1" method="post" action="/controllers/controllerForm.php">
+                    <input type="text" name="nome" id="nome" placeholder="Nome"/>
+                    <input type="tel" name="telefone" id="telefone" placeholder="Telefone"/>
+                    <input type="email" name="email" id="email" placeholder="E-mail"/>
+                </form>
+            </div>
         )
     }
-
 }
